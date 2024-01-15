2ef3dfbd85a2c3c38461a91d4ff1635fc23d2c3f
PUSARLA TARUN VAMSI <pusarlatarunvamsi@gmail.com>
2024-01-15 08:05:31 +0530

    Add annotation processor dependecy for lombok to work'

--- src/main/java/com/vamsi/todo/domain/User.java
+++ src/main/java/com/vamsi/todo/domain/User.java
@@ -1,23 +1,15 @@
 package com.vamsi.todo.domain;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("user")
+@Getter
+@Setter
 public class User {
     @Id
     private String userId;
     private String userName;
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
 }
